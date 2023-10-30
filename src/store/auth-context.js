facce8e542cd0ed549d9a2ab0a4fcb878fafd2fd
Muhammad Aqib Javed <91584862+AqibNiazi@users.noreply.github.com>
2023-10-30 19:03:03 +0500

    All states updated to Global store

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,6 +1,38 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const AuthContext = createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
+
+export const AuthContextProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const loginStatus = localStorage.getItem("isLoggedIn");
+    console.log("key value", loginStatus);
+    if (loginStatus === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
 export default AuthContext;
